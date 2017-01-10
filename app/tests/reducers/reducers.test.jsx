0a74f42b0db1fe88d793bc1d7dee4b1a0e888e7a
Timur Khatypov <t.khatypov@gmail.com>
2017-01-10 21:15:09 +0300

    Refactor app to work with local storage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -38,24 +38,41 @@ describe('Reducers', () => {
       expect(res.length).toEqual(1);
       expect(res[0].text).toEqual(action.text);
     });
-  });
 
-  it('should toggle todo', () => {
-    var todos = [{
-      id: '123',
-      text: 'Something',
-      completed: true,
-      createdAt: 123,
-      completedAt: 125
-    }];
-    var action = {
-      type: 'TOGGLE_TODO',
-      id: '123'
-    };
-    var res = reducers.todosReducer(df(todos), df(action));
+    it('should toggle todo', () => {
+      var todos = [{
+        id: '123',
+        text: 'Something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: '123'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+      
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
+
+    it('should add existing todos', () => {
+      var todos = [{
+        id: '111',
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }];
+      var action = {
+        type: 'ADD_TODOS',
+        todos
+      };
+      var res = reducers.todosReducer(df([]), df(action));
     
-    expect(res[0].completed).toEqual(false);
-    expect(res[0].completedAt).toEqual(undefined);
+      expect(res.length).toEqual(1);
+      expect(res[0]).toEqual(todos[0]);
+    });
   });
-
 });
